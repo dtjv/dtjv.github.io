77635899c3fe5812914b0007ec99a7e9954f4946
David Valles <davidtjvalles@gmail.com>
2020-06-02 10:47:07 -0700

    Scaffold basic Gatsby site

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PageNotFound = () => (
+  <h1>Sorry. We can't find that page.</h1>
+)
+
+export default PageNotFound
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import SEO from '../components/SEO'
+
+const IndexPage = () => (
+  <div>
+    <SEO title="Home" />
+    <h1>Hello World</h1>
+  </div>
+)
+
+export default IndexPage
