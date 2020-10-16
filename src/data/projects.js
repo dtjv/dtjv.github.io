05e768c0ecc70739ff68dca3a3c564866fd0831a
David Valles <davidtjvalles@gmail.com>
2020-10-16 09:38:51 -0700

    Add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    name: 'Maximus',
+    description: '💪 An ultra minimal workout tracker.',
+    meta: {
+      stack: ['React', 'React-Bootstrap', 'Chart.js'],
+    },
+    repositoryUrl: 'https://github.com/dtjv/maximus',
+    deploymentUrl: 'https://maximus-dtjv.netlify.app/',
+  },
+  {
+    name: 'Website: Richard Gullion',
+    description: '🎨 A website for artist, Richard Gullion',
+    meta: {
+      stack: ['Gatsby', 'React', 'Tailwind CSS'],
+    },
+    repositoryUrl: 'https://github.com/dtjv/rg-ui',
+    deploymentUrl: 'https://rich-gullion.netlify.app/',
+  },
+  {
+    name: 'UI: Colleen Valles',
+    description: '✏️  A website UI concept for writer, Colleen Valles ',
+    meta: {
+      stack: ['CSS', 'Pug'],
+    },
+    repositoryUrl: 'https://github.com/dtjv/cv-ui',
+    deploymentUrl: 'https://dtjv.io/cv-ui/',
+  },
+]
