c74fa468a78f99c3dc305ed9c535a95e0079dcb8
David Valles <davidtjvalles@gmail.com>
2021-02-01 06:08:03 -0800

    Fix undefined style corner case

--- src/components/Headings.js
+++ src/components/Headings.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const H1 = ({ classes, children }) => (
   <h1
-    className={`${classes} text-3xl font-extrabold tracking-tight text-gray-800 leading-9 sm:leading-10 sm:text-4xl md:text-5xl md:leading-14`}
+    className={`${
+      classes ?? ''
+    } text-3xl font-extrabold tracking-tight text-gray-800 leading-9 sm:leading-10 sm:text-4xl md:text-5xl md:leading-14`}
   >
     {children}
   </h1>
@@ -10,7 +12,9 @@ export const H1 = ({ classes, children }) => (
 
 export const H2 = ({ classes, children }) => (
   <h2
-    className={`${classes} text-2xl font-bold tracking-tight text-gray-800 leading-8`}
+    className={`${
+      classes ?? ''
+    } text-2xl font-bold tracking-tight text-gray-800 leading-8`}
   >
     {children}
   </h2>
