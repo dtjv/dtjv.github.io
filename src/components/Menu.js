b569d9e348d345f6c6b1135c5fbb49a189e21f42
David Valles <davidtjvalles@gmail.com>
2021-02-12 07:29:54 -0800

    Fix menu close btn position

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ export const Menu = ({ showMenu, onClick }) => {
     <div
       className={`${
         showMenu ? 'fixed' : 'hidden'
-      } inset-0 z-40 h-screen bg-black w-full bg-opacity-25`}
+      } inset-0 z-40 h-full bg-black w-full bg-opacity-25`}
     >
       <div className="h-full px-5 mx-auto mr-32 antialiased bg-white sm:px-8 md:px-12 lg:px-0">
         <Container className="flex flex-col space-y-4">
