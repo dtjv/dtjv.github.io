02dab375a6e1c06b84170f3a038ea6db8e5c342a
David Valles <davidtjvalles@gmail.com>
2021-08-04 15:11:13 -0700

    Add GitHub icon. Remove articles link

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,10 +3,11 @@ import { Link } from 'gatsby'
 
 import MeImg from '../images/me.jpg'
 import { MenuIcon } from './icons/Menu'
+import { GitHubIcon } from './icons/GitHub'
 
 const Nav = ({ onClick }) => {
   return (
-    <div className="flex items-center justify-between py-8 text-gray-800">
+    <div className="flex items-center border-b justify-between py-8 text-gray-800">
       <Link to="/" aria-label="link to home page">
         <div className="flex flex-row items-center">
           <img
@@ -21,19 +22,16 @@ const Nav = ({ onClick }) => {
         className="w-6 h-6 sm:hidden"
         onClick={() => onClick((prevState) => !prevState)}
       />
-      <div className="hidden sm:block space-x-4">
-        <Link
-          to="/articles"
-          className="font-bold text-gray-500 text-normal hover:underline"
-        >
-          Articles
-        </Link>
+      <div className="hidden sm:flex space-x-4">
         <Link
           to="/projects"
           className="font-bold text-gray-500 text-normal hover:underline"
         >
           Projects
         </Link>
+        <a href="https://github.com/dtjv">
+          <GitHubIcon className="w-6 h-6 ml-3 text-color-800 hover:text-blue-400" />
+        </a>
       </div>
     </div>
   )
