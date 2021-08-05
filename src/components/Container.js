afa8d10d374669ef1bdada03a44e0c2915636c0f
David Valles <davidtjvalles@gmail.com>
2021-08-04 21:22:16 -0700

    Stop rendering undefined css class

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const Container = ({ children, className, ...props }) => (
-  <div className={`py-8 sm:py-10 ${className}`} {...props}>
+  <div className={`py-8 sm:py-10 ${className ?? ''}`} {...props}>
     {children}
   </div>
 )
