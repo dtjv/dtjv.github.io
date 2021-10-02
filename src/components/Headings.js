5713e91c96ab146d36c472d53c232d7752fea84e
David Valles <davidtjvalles@gmail.com>
2021-10-01 21:58:27 -0700

    Remove invalid css class

--- src/components/Headings.js
+++ src/components/Headings.js
@@ -4,7 +4,7 @@ export const H1 = ({ classes, children }) => (
   <h1
     className={`${
       classes ?? ''
-    } text-3xl font-extrabold tracking-tight text-gray-800 leading-9 sm:leading-10 sm:text-4xl md:text-5xl md:leading-14`}
+    } text-3xl font-extrabold tracking-tight text-gray-800 leading-9 sm:leading-10 sm:text-4xl md:text-5xl md:leading-none`}
   >
     {children}
   </h1>
