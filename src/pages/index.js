acca54af9871935cb9f65740cdc2751a9bc9a9a4
David Valles <davidtjvalles@gmail.com>
2021-10-27 09:05:37 -0700

    Update intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, useStaticQuery, Link } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import { Layout } from '../components/Layout'
 import { Seo } from '../components/Seo'
@@ -51,21 +51,18 @@ const IndexPage = () => {
       <Container>
         <div className="prose max-w-none">
           <p className="text-xl text-gray-500 sm:text-2xl">
-            Hello! I’m David, a software developer based in Oregon. I build
-            tools and applications using React, TypeScript and many other
-            interesting libraries found in the JavaScript/Node.js ecosystem.
+            Hello! I’m David. I build full-stack web applications using React,
+            TypeScript, MongoDB, and many other interesting libraries in the
+            JavaScript and Node.js ecosystem.
           </p>
           <p className="text-xl text-gray-500 sm:text-2xl">
             On this website I share my thoughts on programming - from coding
-            experiences, to <Link to="/projects">projects</Link> I’ve built and
-            even a bit of computer science basics. I hope you find my content
-            useful.
+            experiences, to projects I’ve built and even a bit of computer
+            science basics. I hope you find my content useful.
           </p>
           <div className="text-xl text-gray-500 sm:text-2xl">
-            <div className="text-gray-900 font-bold">Update...</div>
             <div>
-              I am looking for work! Preferrably Node.js based - Fullstack,
-              Frontend or Backend - I like it all. You may reach me by{' '}
+              Thanks for visiting! You can reach me at{' '}
               <a href="mailto:davidtjvalles@gmail.com">email</a> or{' '}
               <a href="https://twitter/davidtjvalles">@davidtjvalles</a>.
             </div>
