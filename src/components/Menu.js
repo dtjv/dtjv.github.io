a0039fb9d7265c82d13682f6ea9f30c0f4e00bcf
David Valles <davidtjvalles@gmail.com>
2021-11-18 15:51:03 -0800

    Fix mobile menu
    1. Make mobile menu match desktop menu
    1. Add pointer cursor to close menu icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -31,15 +31,15 @@ export const Menu = ({ location, showMenu, onClick }) => {
     >
       <div className="h-full px-5 mx-auto mr-32 antialiased bg-white sm:px-8 md:px-12 lg:px-0">
         <Container className="flex flex-col space-y-4">
-          <MenuLink to="/articles" text="Articles" />
           <MenuLink to="/projects" text="Projects" />
+          <MenuLink to="/resume" text="Resume" />
         </Container>
       </div>
-      <div className="absolute bottom-0 right-0 p-4 m-6 text-white bg-black rounded-full">
-        <CloseIcon
-          className="w-6 h-6"
-          onClick={() => onClick((prevState) => !prevState)}
-        />
+      <div
+        className="cursor-pointer absolute bottom-0 right-0 p-4 m-6 text-white bg-black rounded-full"
+        onClick={() => onClick((prevState) => !prevState)}
+      >
+        <CloseIcon className="w-6 h-6" />
       </div>
     </div>
   )
